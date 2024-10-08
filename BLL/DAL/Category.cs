3f554abac75dc49b2543fd6a1a4e3bbeb7e33a82
irmak <irmak@LAPTOP-EVH350JF>
2024-10-08 17:20:17 +0300

    Proje dosyası ekle.

---
+++ BLL/DAL/Category.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+using System.Collections.Generic;
+
+namespace BLL.DAL
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
